aa31cfad06b1bcc9bdf54933db4233a7ad118800
김휘진 <gimhwijin@huijinui-MacBookPro.local>
2026-02-10 13:33:38 +0900

    Add Global ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { SelectionProvider } from './contexts/SelectionContext';
 import Navbar from './components/Navbar';
@@ -13,22 +14,24 @@ function App() {
     <LanguageProvider>
       <SelectionProvider>
         <Router>
-          <div className="min-h-screen bg-[#f5f5f7] text-[#1d1d1f] font-sans">
-            <Navbar />
+          <ErrorBoundary>
+            <div className="min-h-screen bg-[#f5f5f7] text-[#1d1d1f] font-sans">
+              <Navbar />
 
-            {/* Main Content Area */}
-            <main className="pt-16 min-h-screen pb-24">
-              <Routes>
-                <Route path="/" element={<InfluencerSearch />} />
-                <Route path="/influencers" element={<InfluencerSearch />} />
-                <Route path="/venue" element={<VenueSearch />} />
-                <Route path="/my-selections" element={<MySelections />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
-            </main>
+              {/* Main Content Area */}
+              <main className="pt-16 min-h-screen pb-24">
+                <Routes>
+                  <Route path="/" element={<InfluencerSearch />} />
+                  <Route path="/influencers" element={<InfluencerSearch />} />
+                  <Route path="/venue" element={<VenueSearch />} />
+                  <Route path="/my-selections" element={<MySelections />} />
+                  <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+              </main>
 
-            <FloatingBar />
-          </div>
+              <FloatingBar />
+            </div>
+          </ErrorBoundary>
         </Router>
       </SelectionProvider>
     </LanguageProvider>
